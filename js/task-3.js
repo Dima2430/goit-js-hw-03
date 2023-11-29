c56590f88c9a5bdd8976ab77cac61e57d86418ba
Dima2430 <dima.volkkom@gmail.com>
2023-11-29 19:29:35 +0100

    did all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,10 @@
-function checkForSpam(message) {
-    message = message.toLowerCase();
-    if (message.includes("spam")) {
-        return true;
-    } else if (message.includes("sale")) {
-        return true;
-    } else {
-        return false;
+function filterArray(numbers, value) {
+    const filteredNumbers = [];
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > value) {
+            filteredNumbers.push(numbers[index]);
+        }
+        
     }
+    return filteredNumbers;
 }
-
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
